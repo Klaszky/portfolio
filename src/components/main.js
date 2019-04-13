48a7d40eefafa8852cf9a5a1e4e32afcb6bde7ce
Joseph Klaszky <jfklaszky@gmail.com>
2019-04-12 21:07:21 -0400

    mdl template used to basic structure -- basic layout is done

---
+++ src/components/main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import LandingPage from './landingPage';
+import Resume from './resume';
+import AboutMe from './aboutMe';
+import Projects from './projects';
+import Contact from './contact';
+
+const Main = () => (
+  <Switch>
+    <Route exact path='/' component={LandingPage} />
+    <Route path='/resume' component={Resume} />
+    <Route path='/aboutMe' component={AboutMe} />
+    <Route path='/projects' component={Projects} />
+    <Route path='/contact' component={Contact} />
+  </Switch>
+
+);
+
+export default Main;
